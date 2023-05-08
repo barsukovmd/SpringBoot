d7aeccf80ccd30dc8d0f8594bb436e54e9e5c5c9
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-05-08 14:21:06 -0400

    update

--- Lesson42SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/config/SpringConfig.java
+++ Lesson42SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/config/SpringConfig.java
@@ -60,7 +60,6 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8");
-
         registry.viewResolver(resolver);
     }
 
