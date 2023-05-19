3cd340a30b5dac72d5f5376d2aebdf54ec984e8c
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-05-18 23:27:26 -0400

    hibernate test

--- Lesson44HibernateOneToMany/src/main/java/springboothibernate/onetomany/app/Lesson44HibernateOneToManyApplication.java
+++ Lesson44HibernateOneToMany/src/main/java/springboothibernate/onetomany/app/Lesson44HibernateOneToManyApplication.java
@@ -4,19 +4,26 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springboothibernate.onetomany.models.Item;
 import springboothibernate.onetomany.models.Person;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Lesson44HibernateOneToManyApplication {
 
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         Session session;
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             session = sessionFactory.getCurrentSession();
-
-
             session.beginTransaction();
+
+            Person person = session.get(Person.class, 3);
+            System.out.println(person);
+            List<Item> itemList = person.getItemList();
+            System.out.println(itemList);
+
             session.getTransaction().commit();
         }
     }
